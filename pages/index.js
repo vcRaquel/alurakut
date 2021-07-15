98bcb0d6fe3a002ecf723788ca1cb6e27a07a3a1
vcRaquel <vc.raquel@gmail.com>
2021-07-15 16:54:49 -0300

    terceira aula

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,27 @@ function ProfileSidebar(propriedades){
 //   color: ${({ theme }) => theme.colors.primary};
 // `
 
-
+function ProfileRelationsBox(propriedades){
+  return(
+    <ProfileRelationsBoxWrapper >
+        <h2 className="smallTitle">
+          {propriedades.title} ({propriedades.items.length})
+        </h2>
+        <ul>
+          {/* {seguidores.map((itemAtual) => {
+            return (
+              <li key={itemAtual}>
+                <a href={`https://github.com/${itemAtual.png}`}>
+                  <img src={itemAtual.image} />
+                  <span>{itemAtual.title}</span>
+                </a>
+              </li>
+            )
+          })} */}
+        </ul>
+      </ProfileRelationsBoxWrapper >
+  )
+}
 
 export default function Home() {
   const [comunidades, setComunidades] = React.useState([{
@@ -46,6 +66,22 @@ export default function Home() {
     'marcobrunodev',
     'jakeliny'
   ]
+
+    const [seguidores, setSeguidores] = React.useState([])
+  // 0 - Pegar o array de dados do github
+  
+    React.useEffect(function(){
+    fetch('https://api.github.com/users/vcRaquel/followers')
+    .then(function(respostaDoServidor){
+      return respostaDoServidor.json();
+    })
+    .then(function(respostaCompleta){
+      setSeguidores(respostaCompleta);
+    })
+    }, [])
+
+    console.log('seguidores antes do return', seguidores)
+  // 1 - Criar um box que vai ter um map, baseado nos items do array que pegamos do Github
 
 
   return (
@@ -112,6 +148,7 @@ export default function Home() {
 
 
     <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+      <ProfileRelationsBox title="Seguidores" items={seguidores} />    
       <ProfileRelationsBoxWrapper >
         <h2 className="smallTitle">
           Comunidades ({comunidades.length})
